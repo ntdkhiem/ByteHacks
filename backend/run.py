d9584fcc2f9c546f702b10a62c8cbfec5c642ed1
ntdkhiem <ppkhiemnguyen@gmail.com>
2020-08-01 13:16:56 -0400

    :zap: add cli commands for convenience (v0.1)

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,35 @@
+import click
+from flask.cli import with_appcontext
 from application import create_app, db
 from application.models import User, Gender
 
 app = create_app()
+
+@app.cli.command('init-db')
+# @with_appcontext
+def init_db_command():
+    db.create_all()
+    click.echo('Initialized database')
+
+@app.cli.command('create-user')
+@click.argument('email') 
+@click.argument('password') 
+def create_user(email, password):
+    u = User(
+        first_name='Byte',
+        last_name='Hacks',
+        # dob=datetime.now(),
+        gender=Gender.MALE,
+        location='ma',
+        email=email
+    )
+    u.set_password(password)
+    db.session.add(u)
+    db.session.commit()
+    click.echo(f'Added {email} to the database')
+
+
+    
 
 @app.shell_context_processor
 def make_shell_context():
