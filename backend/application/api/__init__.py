592f0228e1efde1a47bab91b3d8f92f532c0a5c2
ntdkhiem <ppkhiemnguyen@gmail.com>
2020-08-02 15:53:09 -0400

    rename 'backend' to 'services'

--- backend/application/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from firebase_admin import firestore
-
-api = Blueprint('Api', __name__)
-fdb = firestore.client()
-jobs_ref = fdb.collection('jobs')
-
-from . import routes
--- backend/application/dashboard/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-dashb = Blueprint('Dashboard', __name__)
-
-from . import routes
