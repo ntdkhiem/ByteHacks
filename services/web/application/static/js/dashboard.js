7b1cb77e86c3b99d6e82409a9ef73de3f81e1112
ntdkhiem <ppkhiemnguyen@gmail.com>
2020-08-02 23:31:27 -0400

    :sparkles: style frontend

--- services/web/application/static/js/dashboard.js
+++ services/web/application/static/js/dashboard.js
@@ -5,17 +5,31 @@ socketio = io.connect(serverURL)
 socketio.on('connect', () => {
     console.log('Server connected!')
 
-    socket.on('messages', (data) => {
+    socketio.on('message', (data) => {
         console.log('received messages', data)
+        if (data) {
+            if (data.is_full) {
+                document.querySelector('#' + data.id).remove()
+            }
+            else {
+                document.querySelector('#' + data.id)
+                        .querySelector('.workers').textContent = data.total_workers   
+            }
+        }
     })
 })
 
 
 document.querySelectorAll('#btnRegister').forEach(btn => {
     btn.addEventListener('click', (e) => {
+        e.target.disabled = true
         fetch(`http://localhost/dashboard/job/${e.target.parentNode.id}`, {
             method: 'post'
-        }).then(res => res.status == 200)
+        }).then(res => res.json())
+        .then(data => {
+            console.log(data)
+            console.log('Signed UP')
+        })
         .catch(err => {
             console.error(err)
         })
