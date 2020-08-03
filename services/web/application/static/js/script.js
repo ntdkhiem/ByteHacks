7f5a75c1aa730ddc1e173d378a186c87ceb1f2f7
Kevin Nguyen <Ppkhiemnguyen@gmail.com>
2020-08-03 16:42:28 -0400

    :zap: Add validation for login and register forms

--- services/web/application/static/js/script.js
+++ services/web/application/static/js/script.js
@@ -8,3 +8,27 @@ window.addEventListener("scroll",function(){
   let background = document.querySelector("header");
   background.style.backgroundPosition = `-${pageY * .25}px -100px`
 })
+
+window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation');
+    // Loop over them and prevent submission
+    forms.forEach(form =>{
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    })
+    // var validation = Array.prototype.filter.call(forms, function(form) {
+    //   form.addEventListener('submit', function(event) {
+    //     if (form.checkValidity() === false) {
+    //       event.preventDefault();
+    //       event.stopPropagation();
+    //     }
+    //     form.classList.add('was-validated');
+    //   }, false);
+    // });
+  }, false);
